bb0cc4b920ab76d3e92cc001f80aef31dd3a04d8
Kenneth Angell <kangell@ancestry.com>
2015-06-22 21:11:16 -0600

    Bootstrap using Spring starter.

---
+++ src/test/java/phone/service/PhoneServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package phone.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PhoneServiceApplication.class)
+@WebAppConfiguration
+public class PhoneServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
